49c443d45d9c0486fef19e7251adf7b4c2ca4fc2
yejishin-hub <sinyeji958@gmail.com>
2026-02-18 13:27:25 +0900

    2026-02-18 React 11-1. styled-components로 스타일링하기

--- src/pages/CompletionPage/index.js
+++ src/pages/CompletionPage/index.js
@@ -1,5 +1,17 @@
+import styled from 'styled-components';
+
+const a = 12;
+
 function CompletionPage() {
-  return <div>Completion Page</div>;
+  return <CompletionPageContainer>Completion Page</CompletionPageContainer>;
 }
+
+const CompletionPageContainer = styled.div`
+  background-color: #cebe89;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: ${a === 1 ? '100vh' : '50vh'};
+`;
 
 export default CompletionPage;
