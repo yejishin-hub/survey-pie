9803f81a75712bcc23d14b1c9fc2664760b7eb74
yejishin-hub <sinyeji958@gmail.com>
2026-02-22 17:32:25 +0900

    2026-02-22 React 13-1. Axios 설치

--- src/pages/CompletionPage/index.js
+++ src/pages/CompletionPage/index.js
@@ -1,11 +1,20 @@
+// import testState from '../../stores/test/atom';
+import axios from 'axios';
 import { useRecoilValue } from 'recoil';
 import styled from 'styled-components';
 
-// import testState from '../../stores/test/atom';
 import testWithComma from '../../stores/test/testWithComma';
 
 function CompletionPage() {
   const test = useRecoilValue(testWithComma);
+
+  // CORS 정책으로 막힘
+  // axios.get('https://www.google.com').then((res) => {
+
+  // [API 서버] https://github.com/hackurity01/survey-pie-server
+  axios.get('http://localhost:3001/surveys').then((res) => {
+    console.log('response: ', res.data);
+  });
 
   return <CompletionPageContainer>{test}</CompletionPageContainer>;
 }
